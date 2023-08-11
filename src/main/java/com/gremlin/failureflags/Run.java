b7d514e902f800e1adfc4fe3e0f49e5625ca81cb
pmzerbini <paola.mello.zerbini@gmail.com>
2023-08-11 16:41:38 -0400

    update to run

--- src/main/java/com/gremlin/failureflags/Run.java
+++ src/main/java/com/gremlin/failureflags/Run.java
@@ -6,38 +6,39 @@ import com.gremlin.failureflags.models.PrototypeObject;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class Run {
     static Experiment experiment;
     static PrototypeObject defaultBehavior = new PrototypeObject();
-    public static void ifExperimentActive(String name, Map<String, Object> labels, boolean debug) throws IOException {
+    public static boolean ifExperimentActive(String name, Map<String, Object> labels, boolean debug) throws IOException {
 
         if(debug){
-            throw new RuntimeException("ifExperimentActive " + name + " " + labels);
+            Logger.getLogger("ifExperimentActive " + name + " " + labels);
         }
 
         try {
             experiment = Fetch.fetchExperiment(name, labels, debug);
         } catch (Exception ignore) {
             if (debug) {
-                throw new RuntimeException("Unable to fetch experiment: " + ignore.getMessage());
+                Logger.getLogger("Unable to fetch experiment: " + ignore.getMessage());
             }
         }
 
         if (experiment == null) {
             if (debug) {
-                throw new RuntimeException("no experiment for " + name + " " + labels);
+                Logger.getLogger("no experiment for " + name + " " + labels);
             }
         }
 
         if (debug) {
-            throw new RuntimeException("fetched experiment " + experiment);
+            Logger.getLogger("fetched experiment " + experiment);
         }
         double dice = Math.random();
 
         if (experiment.getRate() != null && experiment.getRate() >= 0 && experiment.getRate() <= 1 && dice > experiment.getRate()) {
             if (debug) {
-                throw new RuntimeException("probabilistically skipped " + new RuntimeException());
+                Logger.getLogger("probabilistically skipped " + new RuntimeException());
             }
 
         }
@@ -46,10 +47,11 @@ public class Run {
                 Fault.delayedDataOrException(experiment, defaultBehavior);
             } else {
                 Fault.delayedException(experiment);
+                debug = true;
             }
         } catch (Exception behaviorError) {
             if (debug) {
-                throw new RuntimeException("provided behavior error: " + behaviorError);
+                Logger.getLogger("provided behavior error: " + behaviorError);
             }
             throw behaviorError;
         }
